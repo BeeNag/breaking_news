b7c3701d812f1cfad1fb84e5b2a368c74c961787
BeeNag <bnaglerose88@gmail.com>
2016-01-18 21:15:14 +0000

    Yellow flash not working

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,25 +1,33 @@
 $(document).on('ready page:ready', function () {
     if($("#articles-list").length > 0) {
-        $.ajax({
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            url: "/articles.json",
-            dataType: "json",
-            success: function (articles) {
-
-                var innerHtml = "";
-                for(var i = 0; i < articles.length; i++) {
-                    innerHtml += "<div>";
-                    innerHtml += "<div><h4>" + articles[i].headline + "</h4></div>";
-                    innerHtml += "<div>" + articles[i].body + "</div>";
-                    innerHtml += "</div>";
-                    innerHtml += "<hr />";
-                }
-                $( "#articles-list" ).html(innerHtml);
-            },
-            error: function (){
-                window.alert("There was an error!");
-            }
-        });
+        fetchArticles();
     } 
 });
+
+function fetchArticles() {
+    console.log('Working...');
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: "/articles.json",
+        dataType: "json",
+        success: function (articles) {
+
+            var innerHtml = "";
+            for(var i = 0; i < articles.length; i++) {
+                innerHtml += "<div class='highlight'>";
+                innerHtml += "<div><h4>" + articles[i].headline + "</h4></div>";
+                innerHtml += "<div>" + articles[i].body + "</div>";
+                innerHtml += "</div>";
+                innerHtml += "<hr />";
+            }
+            $( "#articles-list" ).html(innerHtml);
+            $( "#articles-list" ).effect("highlight", {}, 1500);
+
+            // setTimeout(fetchArticles, 5000);
+        },
+        error: function (){
+            window.alert("There was an error!");
+        }
+    });
+}
